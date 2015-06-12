02957c388a04a342adaa5a4c190ba2595b6aae2c
Mustafa Gezen <mustiigezen@gmail.com>
2015-06-12 05:38:14 +0200

    Create undo.js

---
+++ undo.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         undo-github
+// @namespace    io.mstg.undo-github
+// @version      0.1
+// @description  wow undo the header changes to github
+// @author       Mustafa Gezen
+// @match        https://github.com/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/mstg/undo-github/master/undo.js
+// @updateURL    https://raw.githubusercontent.com/mstg/undo-github/master/undo.js
+// ==/UserScript==
+
+$(document).ready(function(){
+    $("a[href='/pulls']").attr("href", "/explore").text("Explore");
+    $("a.dropdown-item[href='/explore']").attr("href", "/pulls").text("Pull requests");
+});
